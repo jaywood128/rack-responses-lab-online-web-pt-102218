de25d0d561a844312fd707e08bfdd5eeeb6c59df
Johnathon Wood <jaywood128@gmail.com>
2019-01-30 20:13:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     time = Time.new
     if time.hour < 12
       resp.write "Good Morning!"
-    elsif time.hour >= 12
+    else time.hour >= 12
       resp.write "Good Evening!"
     end
   end
